ba8461db3a08d8af1d0bf68a01140ea11b7d6403
Reggie <reggie.escobar94@gmail.com>
2025-02-14 17:02:32 +0800

    feat: add configuration for API URL and reconnection timeout

--- api/src/config/config.ts
+++ api/src/config/config.ts
@@ -2,4 +2,5 @@ export const config = {
   roomExpiration: 24 * 60 * 60, // 24 hours in seconds
   playerColor: "#0000FF", // default blue color for host
   playerLimit: 5, // maximum number of players per game
+  reconnectionTimeout: 10000, // 10 seconds to allow for reconnection
 };
